e2309b1cf41eed5ef0e83f6abf206661f9f511f0
kulakowka <kulakowka@gmail.com>
2016-12-22 02:25:57 +0300

    add loadmore prop

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = function execPaginatePlugin (schema, options) {
   schema.query.execPaginate = function (params = {}) {
     const skip = parseInt(params.skip) || 0
     const limit = parseInt(params.limit || options.limit) || 10
+    const loadmore = !!params.loadmore
 
     return this
       .limit(limit)
@@ -9,6 +10,7 @@ module.exports = function execPaginatePlugin (schema, options) {
       .exec()
       .then(data => ({
         data,
+        loadmore,
         skip: skip + limit
       }))
   }
