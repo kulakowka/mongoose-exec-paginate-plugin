30e76a78b70afaf2fbcf9af9b05e83edd9dfa1ba
kulakowka <kulakowka@gmail.com>
2016-12-22 01:54:18 +0300

    fix execPaginate params

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = function execPaginatePlugin (schema, options) {
-  schema.query.execPaginate = function ({ limit, skip }) {
-    skip = parseInt(skip) || 0
-    limit = parseInt(limit || options.limit) || 10
+  schema.query.execPaginate = function (params = {}) {
+    const skip = parseInt(params.skip) || 0
+    const limit = parseInt(params.limit || options.limit) || 10
 
     return this
       .limit(limit)
